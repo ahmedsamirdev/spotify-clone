2d1ae97b4eec100f3a69af0b4777f6553d4c818b
unknown <ahmedsamir551@gmail.com>
2020-12-25 22:53:58 +0200

    update

--- components/spotify.js
+++ components/spotify.js
@@ -15,9 +15,9 @@ export const getTokenFromResponse = () => {
   return window.location.hash
     .substring(1)
     .split("&")
-     .reduce((initial, item) => {
-       var parts = item.split("=");
-       initial[parts[0]] = decodeURIComponent(parts[1]);
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
 
       return initial;
     }, {});
